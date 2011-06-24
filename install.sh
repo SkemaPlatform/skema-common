f1cca79add98f96961e685401ae1ba14415ff4de
Luis Cabellos <cabellos@ifca.unican.es>
2011-06-24 14:13:18 +0200

    add checks in linux scripts
    Ignore-this: 662acfd7ac6df0e10733cc1afe6a26a0
    
    darcs-hash:20110624121318-6831e-7e232e6ea0a2c328bfb85ae9044aeb9a6386b83d.gz

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 runhaskell Setup configure --user
+if [ "$?" = "1" ]
+then
+    exit 1
+fi
 runhaskell Setup build
+if [ "$?" = "1" ]
+then
+    exit 1
+fi
 runhaskell Setup haddock
+if [ "$?" = "1" ]
+then
+    exit 1
+fi
 runhaskell Setup install
+if [ "$?" = "1" ]
+then
+    exit 1
+fi
--- test.sh
+++ test.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 runhaskell Setup configure --user --flags=testing
+if [ "$?" = "1" ]
+then
+    exit 1
+fi
 runhaskell Setup build
+if [ "$?" = "1" ]
+then
+    exit 1
+fi
 rm -f *.tix
 dist/build/skema-common-test/skema-common-test
+if [ "$?" = "1" ]
+then
+    exit 1
+fi
 hpc report *.tix
 hpc markup *.tix
+set -e
