8144f2015794a077267f63c4a8b61c8c1dbd71b2
Luis Cabellos <cabellos@ifca.unican.es>
2011-06-28 10:16:32 +0200

    move html output to dist
    Ignore-this: 4300ef51db4f782e9857897ed1834933
    
    darcs-hash:20110628081632-6831e-7da6fc46b136c445969b1c8ffe1820ecef9c7be2.gz

--- test.sh
+++ test.sh
@@ -5,17 +5,25 @@ if [ "$?" = "1" ]
 then
     exit 1
 fi
+
 runhaskell Setup build
 if [ "$?" = "1" ]
 then
     exit 1
 fi
+
 rm -f *.tix
 dist/build/skema-common-test/skema-common-test
 if [ "$?" = "1" ]
 then
     exit 1
 fi
+
+HPCDIR="dist/hpc-html"
+if [ ! -d $HPCDIR ]
+then
+    mkdir -p $HPCDIR
+fi
 hpc report *.tix
-hpc markup *.tix
+hpc markup --destdir=$HPCDIR *.tix
 set -e
