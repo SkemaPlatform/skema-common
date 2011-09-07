9ab663ae126c94baa75db972a7e719c2cc822d13
Luis Cabellos <cabellos@ifca.unican.es>
2011-09-07 10:53:31 +0200

    add full conversion of types from/to ByteString

--- src/Skema/DataValue.hs
+++ src/Skema/DataValue.hs
@@ -22,8 +22,9 @@ module Skema.DataValue( DataValue(..) )
 import Data.Word( Word8, Word16, Word32, Word64 )
 import Data.Int( Int8, Int16, Int32, Int64 )
 import Data.Bits( (.&.), (.|.), shiftR, shiftL )
+import Data.Binary.IEEE754( floatToWord, wordToFloat )
 import qualified Data.ByteString as B( 
-  ByteString, pack, index, singleton )
+  ByteString, empty, pack, index, singleton )
 
 -- -----------------------------------------------------------------------------
 data DataValue = DVchar Int8 | DVuchar Word8 | DVshort Int16 
@@ -32,11 +33,13 @@ data DataValue = DVchar Int8 | DVuchar Word8 | DVshort Int16
                deriving( Show )
                        
 -- -----------------------------------------------------------------------------
-class ToByteString a where
-  toByteString_le :: a -> B.ByteString
-  toByteString_be :: a -> B.ByteString
-  fromByteString_le :: B.ByteString -> a
-  fromByteString_be :: B.ByteString -> a
+class Num a => ToByteString a where
+  toByteString_le, toByteString_be :: a -> B.ByteString
+  toByteString_le = const B.empty
+  toByteString_be = const B.empty
+  fromByteString_le, fromByteString_be :: B.ByteString -> a
+  fromByteString_le = const 0
+  fromByteString_be = const 0
   
 instance ToByteString Int8 where
   toByteString_le = B.singleton . fromIntegral
@@ -138,4 +141,52 @@ instance ToByteString Word32 where
       c = (fromIntegral $ w `B.index` 1) `shiftL` 16
       d = (fromIntegral $ w `B.index` 0) `shiftL` 24
 
+instance ToByteString Word64 where
+  toByteString_le w = B.pack [a,b,c,d,e,f,g,h]
+    where
+      a = fromIntegral $ (w .&. 0xff)
+      b = fromIntegral $ (w .&. 0xff00) `shiftR` 8
+      c = fromIntegral $ (w .&. 0xff0000) `shiftR` 16
+      d = fromIntegral $ (w .&. 0xff000000) `shiftR` 24
+      e = fromIntegral $ (w .&. 0xff00000000) `shiftR` 32
+      f = fromIntegral $ (w .&. 0xff0000000000) `shiftR` 40
+      g = fromIntegral $ (w .&. 0xff000000000000) `shiftR` 48
+      h = fromIntegral $ (w .&. 0xff00000000000000) `shiftR` 56
+  toByteString_be w = B.pack [h,g,f,e,d,c,b,a]
+    where
+      a = fromIntegral $ (w .&. 0xff)
+      b = fromIntegral $ (w .&. 0xff00) `shiftR` 8
+      c = fromIntegral $ (w .&. 0xff0000) `shiftR` 16
+      d = fromIntegral $ (w .&. 0xff000000) `shiftR` 24
+      e = fromIntegral $ (w .&. 0xff00000000) `shiftR` 32
+      f = fromIntegral $ (w .&. 0xff0000000000) `shiftR` 40
+      g = fromIntegral $ (w .&. 0xff000000000000) `shiftR` 48
+      h = fromIntegral $ (w .&. 0xff00000000000000) `shiftR` 56
+  fromByteString_le w = a .|. b .|. c .|. d .|. e .|. f .|. g .|. h
+    where
+      a = fromIntegral $ w `B.index` 0
+      b = (fromIntegral $ w `B.index` 1) `shiftL` 8
+      c = (fromIntegral $ w `B.index` 2) `shiftL` 16
+      d = (fromIntegral $ w `B.index` 3) `shiftL` 24
+      e = (fromIntegral $ w `B.index` 4) `shiftL` 32
+      f = (fromIntegral $ w `B.index` 5) `shiftL` 40
+      g = (fromIntegral $ w `B.index` 6) `shiftL` 48
+      h = (fromIntegral $ w `B.index` 7) `shiftL` 56
+  fromByteString_be w = a .|. b .|. c .|. d .|. e .|. f .|. g .|. h
+    where
+      a = fromIntegral $ w `B.index` 7
+      b = (fromIntegral $ w `B.index` 6) `shiftL` 8
+      c = (fromIntegral $ w `B.index` 5) `shiftL` 16
+      d = (fromIntegral $ w `B.index` 4) `shiftL` 24
+      e = (fromIntegral $ w `B.index` 3) `shiftL` 32
+      f = (fromIntegral $ w `B.index` 2) `shiftL` 40
+      g = (fromIntegral $ w `B.index` 1) `shiftL` 48
+      h = (fromIntegral $ w `B.index` 0) `shiftL` 56
+
+instance ToByteString Float where
+  toByteString_le = toByteString_le . floatToWord
+  toByteString_be = toByteString_be . floatToWord
+  fromByteString_le = wordToFloat . fromByteString_le
+  fromByteString_be = wordToFloat . fromByteString_be
+      
 -- -----------------------------------------------------------------------------
